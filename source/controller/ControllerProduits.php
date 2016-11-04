5c5750d4267ca1d75b0862396d073de3344b6111
todave <todave11@gmail.com>
2016-11-04 11:58:33 +0100

    controler getAll
    fonction getAll

--- source/controller/ControllerProduits.php
+++ source/controller/ControllerProduits.php
@@ -1,4 +1,12 @@
 <?php
+    require_once ('../model/ModelProduis.php');
     
+    class ControllerProduits {
+        
+        public static function readAll() {
+            $tabe_p= Model::getAllProduits();
+            require_once File::build_path(array('view','Produits','Produits.php'));
+        }
+    }
 ?>
 
