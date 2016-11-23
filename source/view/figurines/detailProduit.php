3fd03d178c58187acee12f067227d55a09f1318e
sancheza <alexis.sanchez01@etu.umontpellier.fr>
2016-11-24 00:23:48 +0100

    correction des problemes avec les url

--- source/view/figurines/detailProduit.php
+++ source/view/figurines/detailProduit.php
@@ -1,5 +1,5 @@
  <?php
         echo "Numero Produit: " . htmlspecialchars($f->getNumProduit()) . " Nom Produit: " . htmlspecialchars($f->getNomProduit()) . " Quantité en stock: " . htmlspecialchars($f->getQteStock()) . " Catégorie: " . htmlspecialchars($f->getCategorie()) . " Prix: " . htmlspecialchars($f->getPrix()) . " Poids: " . htmlspecialchars($f->getPoids()) . " Hauteur: " . htmlspecialchars($f->getHauteur()) . " Univers: " . htmlspecialchars($f->getUnivers());
         //echo '<p><a href=http://infolimon/~sancheza/PHP/TD4/index.php?action=delete&imt='.rawurlencode($v->getImmatriculation()).'>       Supprimer voiture</a></p>';
-         echo '</br><p><a href=http://infolimon/~sancheza/eCommerce/source>Retour</a></p>';
+         echo '</br><p><a href=http://infolimon.iutmontp.univ-montp2.fr/~sancheza/eCommerce/source>Retour</a></p>';
         ?>
--- source/view/figurines/list.php
+++ source/view/figurines/list.php
@@ -1,7 +1,7 @@
 
         <?php
         foreach ($tab_f as $f) {
-           echo '<p class="fig" style="display: none;"> N°Figurine: <a href="http://infolimon/~sancheza/eCommerce/source/index.php?action=read&numProduit=' . rawurlencode($f->getNumProduit()) . '">' . htmlspecialchars($f->getNumProduit()) . '</a></p>';
+           echo '<p class="fig" style="display: none;"> N°Figurine: <a href="http://infolimon.iutmontp.univ-montp2.fr/~sancheza/eCommerce/source/index.php?action=read&numProduit=' . rawurlencode($f->getNumProduit()) . '">' . htmlspecialchars($f->getNumProduit()) . '</a></p>';
         }        
         ?>
 
